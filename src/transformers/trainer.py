69a4b7d8253e1af92c21a99b9ecded4cfbfb7317
Vu Anh Khoi <135935358+Ekanara@users.noreply.github.com>
2025-04-19 09:53:19 +0700

    Update trainer.py

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -5274,8 +5274,6 @@ class Trainer:
 
             if torch.is_tensor(num_items_in_batch):
                 num_items_in_batch = num_items_in_batch.to(device)
-
-                print('num_gpu:', args.n_gpu)
                 
                 if self.args.n_gpu > 1 and num_items_in_batch.dim() == 0:
                     # In the DataParallel case, convert the scalar tensor into a 1-dim tensor
